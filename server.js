646647ac6ea6fffabe5c30e4442f81fcf4de3e54
69319010129 <69319010129@techno-chon.ac.th>
2026-07-23 12:25:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const client = await pool.connect();
 const result = await client.query('SELECT * FROM students');
 client.release(); // คนืการเชื่อมต่อเมื่อใช้งานเสร็จ
 // 4. นําขอมูลที่ได้(result.rows) มาประกอบเป็นตาราง HTML
-let html = `<h1>ฐานขอมูลนักศึกษา (ทดสอบการเชื่อมต่อ)</h1>`;
+let html = `<h1>ฐานข้อมูลนักศึกษา (ทดสอบการเชื่อมต่อ)</h1>`;
 html += `<table border="1" cellpadding="10">`;
 html += `<tr><th>69319010129</th><th>jirapon phasuk</th></tr>`;
 // วนลูปนําข้อมูลแต่ละแถวมาแสดง
@@ -26,7 +26,7 @@ html += `<tr><td>${row.student_id}</td><td>${row.student_name}</td></tr>`;
 html += `</table>`;
 res.end(html);
 } catch (err) {
-// กรณเีชื่อมตอไมไดหรือเขียนชื่อตารางผิด
+// กรณเชื่อมต่อไม่ได้หรือเขียนชื่อตารางผิด
 console.error(err);
 res.end(`<h1>เกิดข้อผิดพลาด!</h1><p>${err.message}</p>`);
 }
